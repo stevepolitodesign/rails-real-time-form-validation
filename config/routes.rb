72f2d62b6f7140ca408826a9d03c93c4734ea81c
Steve Polito <info@stevepolitodesign.com>
2021-04-26 07:44:26 -0400

    Create namespaced route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
-  namespace :posts do
-    resources :form_validations
+  namespace :form_validations do
+    resources :posts, only: [:create, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
