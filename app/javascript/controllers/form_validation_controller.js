710bab015bc5798bc725583c498cbb9a9451103a
Steve Polito <info@stevepolitodesign.com>
2021-05-05 20:25:57 -0400

    WIP: Focus on element.

--- app/javascript/controllers/form_validation_controller.js
+++ app/javascript/controllers/form_validation_controller.js
@@ -5,17 +5,16 @@ export default class extends Controller {
   static targets  = [ "form", "output"]
   static values   = { url: String }
 
-  connect() {
-    console.log(this);
-  }
-
-  handleChange() {
+  handleChange(event) {
+    let input = event.target
     Rails.ajax({
       url: this.urlValue,
       type: "POST",
       data: new FormData(this.formTarget),
       success: (data) => {
         this.outputTarget.innerHTML = data;
+        input = document.getElementById(input.id);
+        input.focus();
       },
     })
   }
