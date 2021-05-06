a38854a0a8f5faa14043713c58eaa722a4af22e0
Steve Polito <info@stevepolitodesign.com>
2021-05-05 21:54:08 -0400

    Validate form on edit.

--- app/controllers/form_validations/posts_controller.rb
+++ app/controllers/form_validations/posts_controller.rb
@@ -1,4 +1,12 @@
 class FormValidations::PostsController < PostsController
+  def update
+    @post.assign_attributes(post_params)
+    @post.valid?
+    respond_to do |format|
+      format.json { render partial: 'posts/form', locals: { post: @post }, formats: [:html] } 
+    end
+  end
+  
   def create
     @post = Post.new(post_params)
     @post.validate
