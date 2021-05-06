9408127aa88b41c3a7f2fe6a71765ec9d67c46ba
Steve Polito <info@stevepolitodesign.com>
2021-05-06 07:19:21 -0400

    Revert "Install Turbo."
    This reverts commit 44c42b589cf19f0c9b9c84b2251521adfd7a0b12.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,12 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import "@hotwired/turbo-rails"
+import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
+Turbolinks.start()
 ActiveStorage.start()
 
 import "controllers"
